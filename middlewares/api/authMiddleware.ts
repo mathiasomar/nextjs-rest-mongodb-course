0aa5c33e3a410211f38215765d994d5ae60c1f8f
mathiasomar <ommykmathy@gmail.com>
2025-08-15 15:58:45 +0300

    middlewares

---
+++ middlewares/api/authMiddleware.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const validate = (token: any) => {
+  const validToken = true;
+  if (!validToken || !token) {
+    return false;
+  }
+
+  return true;
+};
+
+export function authMiddleware(request: Request): any {
+  const token = request.headers.get("authorization")?.split(" ")[1];
+
+  return { isValid: validate(token) };
+}
