02586108682d161495a8e5e6bce2eaedd7f2954d
mathiasomar <ommykmathy@gmail.com>
2025-08-15 10:36:36 +0300

    delete user

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -81,3 +81,46 @@ export const PATCH = async (request: Request) => {
     );
   }
 };
+
+export const DELETE = async (request: Request) => {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId) {
+      return new NextResponse(
+        JSON.stringify({ message: "User ID not found" }),
+        { status: 400 }
+      );
+    }
+
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid user ID" }), {
+        status: 400,
+      });
+    }
+
+    await connectDB();
+
+    const deletedUser = await User.findByIdAndDelete(
+      new Types.ObjectId(userId)
+    );
+
+    if (!deletedUser) {
+      return new NextResponse(
+        JSON.stringify({ message: "User not found in the database" }),
+        { status: 404 }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({ message: "User deleted successfully" }),
+      { status: 200 }
+    );
+  } catch (error: unknown) {
+    return new NextResponse(
+      "Error deleting user: " + (error as Error).message,
+      { status: 500 }
+    );
+  }
+};
