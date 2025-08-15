e0dc11f705822259c41a0876e1a7617f68e5795f
mathiasomar <ommykmathy@gmail.com>
2025-08-15 09:28:54 +0300

    get users from db

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,5 +1,16 @@
+import connectDB from "@/lib/db";
+import User from "@/lib/models/user";
 import { NextResponse } from "next/server";
 
-export const GET = () => {
-  return new NextResponse("This is my first API route");
+export const GET = async () => {
+  try {
+    await connectDB();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error: unknown) {
+    return new NextResponse(
+      "Error fetching users: " + (error as Error).message,
+      { status: 500 }
+    );
+  }
 };
