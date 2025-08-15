debf0002ec3debdc654acd067a98344ec907aa4d
mathiasomar <ommykmathy@gmail.com>
2025-08-15 14:40:14 +0300

    update blog

--- app/api/(dashboard)/blogs/[blog]/route.ts
+++ app/api/(dashboard)/blogs/[blog]/route.ts
@@ -73,3 +73,72 @@ export const GET = async (request: Request, context: { params: any }) => {
     );
   }
 };
+
+export const PATCH = async (request: Request, context: { params: any }) => {
+  const blogId = context.params.blog;
+  try {
+    const body = await request.json();
+    const { title, description } = body;
+
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Missing or Invalid user ID" }),
+        { status: 400 }
+      );
+    }
+
+    if (!blogId || !Types.ObjectId.isValid(blogId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Missing or Invalid blog ID" }),
+        { status: 400 }
+      );
+    }
+
+    await connectDB();
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
+    }
+
+    const blog = await Blog.findOne({
+      _id: blogId,
+      user: new Types.ObjectId(userId),
+    });
+
+    if (!blog) {
+      return new NextResponse(JSON.stringify({ message: "Blog not found" }), {
+        status: 404,
+      });
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        title,
+        description,
+      },
+      { new: true }
+    );
+
+    return new NextResponse(
+      JSON.stringify({
+        message: "Blog Updated Successfuly",
+        blog: updatedBlog,
+      }),
+      {
+        status: 200,
+      }
+    );
+  } catch (error: unknown) {
+    return new NextResponse(
+      "Error updating blog: " + (error as Error).message,
+      { status: 500 }
+    );
+  }
+};
