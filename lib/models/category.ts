0196a98585235577d4e96852093cd264893f39fc
mathiasomar <ommykmathy@gmail.com>
2025-08-15 10:41:15 +0300

    category model

---
+++ lib/models/category.ts
@@ -0,0 +1,13 @@
+import { Schema, models, model } from "mongoose";
+
+const categorySchema = new Schema(
+  {
+    title: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+const Category = models.Category || model("Category", categorySchema);
+
+export default Category;
