0978fc23e045a42b3f46a8be0032f0c5f363743f
mathiasomar <ommykmathy@gmail.com>
2025-08-15 10:57:53 +0300

    get categories

---
+++ app/api/(dashboard)/categories/route.ts
@@ -0,0 +1,39 @@
+import connectDB from "@/lib/db";
+import Category from "@/lib/models/category";
+import User from "@/lib/models/user";
+import { NextResponse } from "next/server";
+import { Types } from "mongoose";
+
+export const GET = async (request: Request) => {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "User id not found or invalid user ID" }),
+        { status: 400 }
+      );
+    }
+
+    await connectDB();
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
+    }
+
+    const categories = await Category.find({
+      user: new Types.ObjectId(userId),
+    });
+
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (error: unknown) {
+    return new NextResponse(
+      "Error fetching categories: " + (error as Error).message,
+      { status: 500 }
+    );
+  }
+};
