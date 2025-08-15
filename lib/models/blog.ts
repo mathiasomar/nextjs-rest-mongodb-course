356b962282ff12fc85dd72dbbdba3a5f6e64c58a
mathiasomar <ommykmathy@gmail.com>
2025-08-15 12:19:06 +0300

    blog model

---
+++ lib/models/blog.ts
@@ -0,0 +1,27 @@
+import { Schema, models, model } from "mongoose";
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  },
+  { timestamps: true }
+);
+
+const Blog = models.Blog || model("Blog", blogSchema);
+
+export default Blog;
